821e43416a0248f31b0620b003aa9407974c4d47
wpko <113586127+wpko@users.noreply.github.com>
2025-03-09 06:56:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -51,30 +51,35 @@ def predict_stock(stock_symbol:str,days:int=30):
         return {"error:": str(e)}
 
 @app.get('/plot')
-def plot_stock(stock_symbol:str,days:int=30):
-    time.sleep(2)
-    df = yf.download(stock_symbol,period='60d')
-    actual_price = df['Close'].values
-    data_scaled = scaler.transform(actual_price.reshape(-1,1))
-    x_input = np.array(data_scaled[-60:])
-    x_input = np.reshape(x_input,(1,60,1))
-    predictions=[]
-    for _ in range(days):
-        pred = model.predict(x_input)
-        predictions.append(pred[0][0])
-        x_input = np.roll(x_input,-1,axis=1)
-        x_input[0,-1,0]=pred[0][0]
-    predicted_prices = scaler.inverse_transform(np.array(predictions).reshape(-1,1)).flatten()
-    fig = plt.figure(figsize=(12,6))
-    plt.plot(range(len(actual_price)),actual_price,label="Actual Price",color = 'green')
-    plt.plot(range(len(actual_price),len(actual_price)+days),predicted_prices,label="Predicted Price", color = 'red')
-    plt.xlabel('Days')
-    plt.ylabel('Stock Price')
-    plt.legend()
-    img_buffer = io.BytesIO()
-    plt.savefig(img_buffer,format='png')
-    plt.close(fig)
-    img_buffer.seek(0)
-    img_base64 = base64.b64encode(img_buffer.read()).decode('utf-8')
-    return {"image": f"data:image/png;base64,{img_base64}"}
+def plot_stock(stock_symbol:str,days:int=10):
+    try:
+        time.sleep(2)
+        df = yf.download(stock_symbol,period='60d')
+        if df.empty:
+            return{'error':"Invalid stock symbol or no data available"}
+        actual_price = df['Close'].values[-60:]
+        data_scaled = scaler.transform(df[["Close"]].values)
+        x_input = np.array(data_scaled[-60:])
+        x_input = np.reshape(x_input,(1,60,1))
+        predictions=[]
+        for _ in range(days):
+            pred = model.predict(x_input)
+            predictions.append(pred[0][0])
+            x_input = np.roll(x_input,-1,axis=1)
+            x_input[0,-1,0]=pred[0][0]
+        predicted_prices = scaler.inverse_transform(np.array(predictions).reshape(-1,1)).flatten()
+        fig = plt.figure(figsize=(12,6))
+        plt.plot(range(60),actual_price,label="Actual Price",color = 'green')
+        plt.plot(range(60,60+days),predicted_prices,label="Predicted Price", color = 'red')
+        plt.xlabel('Days')
+        plt.ylabel('Stock Price')
+        plt.legend()
+        img_buffer = io.BytesIO()
+        plt.savefig(img_buffer,format='png')
+        plt.close(fig)
+        img_buffer.seek(0)
+        img_base64 = base64.b64encode(img_buffer.read()).decode('utf-8')
+        return {"image": f"data:image/png;base64,{img_base64}"}
+    except Exception as e:
+        return{"error":str(e)}
 
