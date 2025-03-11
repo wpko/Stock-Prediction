e6741ce405ee1d0da794ee4dc1a0170b30433b5f
wpko <113586127+wpko@users.noreply.github.com>
2025-03-12 06:46:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -57,24 +57,17 @@ def plot_stock(stock_symbol:str,days:int=10):
         df = yf.download(stock_symbol,period='60d')
         if df.empty:
             return{'error':"Invalid stock symbol or no data available"}
-        actual_price = df['Close'].values[-60:]
-        data_scaled = scaler.fit_transform(df[["Close"]].values)
-        x_input = np.array(data_scaled[-60:])
-        x_input = np.reshape(x_input,(1,60,1))
-        predictions=[]
-        for _ in range(days):
-            pred = model.predict(x_input)
-            predictions.append(pred[0][0])
-            x_input = np.roll(x_input,-1,axis=1)
-            x_input[0,-1,0]=pred[0][0]
-        predicted_prices = scaler.inverse_transform(np.array(predictions).reshape(-1,1)).flatten()
-        full_predicted_prices = np.concatenate((actual_price,predicted_prices))
+        prediction = model.predict(x_test)
+        predicted_prices = scaler.inverse_transform(prediction)
+        actual_prices = scaler.inverse_transform(y_test.reshape(-1,1))
         fig = plt.figure(figsize=(12,6))
-        plt.plot(range(60),actual_price,label="Actual Price",color = 'green')
-        plt.plot(range(70),full_predicted_prices,label="Predicted Price", color = 'red')
-        plt.xlabel('Days')
-        plt.ylabel('Stock Price')
-        plt.legend()
+        plt.figure(figsize=(12,6))
+        plt.plot(actual_prices,label = "Actual Price",color='blue')
+        plt.plot(predicted_prices,label="Predicted Price",color = 'red')
+        plt.title(f"{stock_symbol} Stock Price Prediction(LMST)")
+        plt.xlabel("Days")
+        plt.ylabel("Stock Price")
+        plt.lengend()
         img_buffer = io.BytesIO()
         plt.savefig(img_buffer,format='png')
         plt.close(fig)
